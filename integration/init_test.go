195e45d3eca604cf26bec4a907ea1755ee4347ae
Forest Eckhardt <feckhardt@pivotal.io>
2020-07-22 18:26:01 -0400

    Adds an integration test that tests all of the components of the dotnet-core buildpack

---
+++ integration/init_test.go
@@ -0,0 +1,38 @@
+package integration_test
+
+import (
+	"bytes"
+	"path/filepath"
+	"testing"
+	"time"
+
+	"github.com/paketo-buildpacks/packit/pexec"
+	"github.com/sclevine/spec"
+	"github.com/sclevine/spec/report"
+
+	. "github.com/onsi/gomega"
+)
+
+var dotnetCoreBuildpack string
+
+func TestIntegration(t *testing.T) {
+	Expect := NewWithT(t).Expect
+
+	bash := pexec.NewExecutable("bash")
+	buffer := bytes.NewBuffer(nil)
+	err := bash.Execute(pexec.Execution{
+		Args:   []string{"-c", "../scripts/package.sh --version 1.2.3"},
+		Stdout: buffer,
+		Stderr: buffer,
+	})
+	Expect(err).NotTo(HaveOccurred(), buffer.String)
+
+	dotnetCoreBuildpack, err = filepath.Abs("../build/buildpackage.cnb")
+	Expect(err).NotTo(HaveOccurred())
+
+	SetDefaultEventuallyTimeout(10 * time.Second)
+
+	suite := spec.New("Integration", spec.Parallel(), spec.Report(report.Terminal{}))
+	suite("Default", testDefault)
+	suite.Run(t)
+}
