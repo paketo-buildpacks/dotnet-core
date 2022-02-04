d4cafc61c94ebd821a5b23a09f4b93902a33f8c4
Frankie Gallina-Jones <frankieg@vmware.com>
2022-02-04 14:37:53 -0500

    poll container logs with Eventually

--- integration/fdd_test.go
+++ integration/fdd_test.go
@@ -227,10 +227,11 @@ func testFDD(t *testing.T, context spec.G, it spec.S) {
 					Execute(image.ID)
 				Expect(err).NotTo(HaveOccurred())
 
-				containerLogs, err := docker.Container.Logs.Execute(procfileContainer.ID)
-				Expect(err).NotTo(HaveOccurred())
-
-				Expect(containerLogs.String()).To(ContainSubstring("Procfile command"))
+				Eventually(func() string {
+					containerLogs, err := docker.Container.Logs.Execute(procfileContainer.ID)
+					Expect(err).NotTo(HaveOccurred())
+					return containerLogs.String()
+				}).Should(ContainSubstring("Procfile command"))
 			})
 		})
 	})
