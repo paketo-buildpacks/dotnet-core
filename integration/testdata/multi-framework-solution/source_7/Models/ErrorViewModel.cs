a5a3d018b8078b038ea57c67e3ff7b55e22f89fc
Sophie Wigmore <swigmore@vmware.com>
2024-07-03 12:12:13 -0400

    Remove .NET 7 tests (#1107)

--- integration/testdata/multi-framework-solution/source_7/Models/ErrorViewModel.cs
+++
@@ -1,8 +0,0 @@
-namespace source_7_app.Models;
-
-public class ErrorViewModel
-{
-    public string? RequestId { get; set; }
-
-    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-}
---
+++ integration/testdata/source-app-8/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace source_app_8.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
